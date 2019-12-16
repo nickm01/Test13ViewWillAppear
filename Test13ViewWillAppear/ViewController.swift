ea4a04c179a70a9afc5fbe3955ba50666dd7e500
Nick McConnell <nicholasgmcconnell@gmail.com>
2019-12-16 14:19:08 -0800

    Initial Commit

---
+++ Test13ViewWillAppear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test13ViewWillAppear
+//
+//  Created by Nick McConnell on 12/16/19.
+//  Copyright © 2019 Nick McConnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
